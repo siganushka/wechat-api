c7ec97e216f356e13130c7983de8d7dadaa8a249
siganushka <siganushka@gmail.com>
2022-06-07 15:58:34 +0800

    tests: add ticket test cases.

---
+++ tests/Ticket/TicketTest.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\ApiClient\Wechat\Tests\Ticket;
+
+use PHPUnit\Framework\TestCase;
+use Siganushka\ApiClient\Exception\ParseResponseException;
+use Siganushka\ApiClient\RequestOptions;
+use Siganushka\ApiClient\Response\ResponseFactory;
+use Siganushka\ApiClient\Wechat\Ticket\Ticket;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
+use Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class TicketTest extends TestCase
+{
+    public function testResolve(): void
+    {
+        $request = static::createRequest();
+
+        $resolved = $request->resolve(['access_token' => 'foo']);
+        static::assertSame(['type' => 'jsapi', 'access_token' => 'foo'], $resolved);
+        static::assertSame(['access_token', 'type'], $request->getResolver()->getDefinedOptions());
+    }
+
+    public function testSend(): void
+    {
+        $data = [
+            'ticket' => 'test_ticket',
+        ];
+
+        $response = ResponseFactory::createMockResponseWithJson($data);
+
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $httpClient->method('request')->willReturn($response);
+
+        $request = static::createRequest();
+        $request->setHttpClient($httpClient);
+
+        $parsedResponse = $request->send(['access_token' => 'foo']);
+        static::assertSame($data, $parsedResponse);
+    }
+
+    public function testConfigureRequest(): void
+    {
+        $request = static::createRequest();
+        $requestOptions = new RequestOptions();
+
+        $configureRequestRef = new \ReflectionMethod($request, 'configureRequest');
+        $configureRequestRef->setAccessible(true);
+        $configureRequestRef->invoke($request, $requestOptions, $request->resolve(['access_token' => 'foo']));
+
+        static::assertSame('GET', $requestOptions->getMethod());
+        static::assertSame(Ticket::URL, $requestOptions->getUrl());
+        static::assertSame([
+            'query' => [
+                'access_token' => 'foo',
+                'type' => 'jsapi',
+            ],
+        ], $requestOptions->toArray());
+
+        $configureRequestRef->invoke($request, $requestOptions, $request->resolve(['access_token' => 'foo', 'type' => 'wx_card']));
+        static::assertSame([
+            'query' => [
+                'access_token' => 'foo',
+                'type' => 'wx_card',
+            ],
+        ], $requestOptions->toArray());
+    }
+
+    public function testParseResponseException(): void
+    {
+        $this->expectException(ParseResponseException::class);
+        $this->expectExceptionCode(16);
+        $this->expectExceptionMessage('test error');
+
+        $data = [
+            'errcode' => 16,
+            'errmsg' => 'test error',
+        ];
+
+        $response = ResponseFactory::createMockResponseWithJson($data);
+
+        $request = static::createRequest();
+        $parseResponseRef = new \ReflectionMethod($request, 'parseResponse');
+        $parseResponseRef->setAccessible(true);
+        $parseResponseRef->invoke($request, $response);
+    }
+
+    public function testAccessTokenMissingException(): void
+    {
+        $this->expectException(MissingOptionsException::class);
+        $this->expectExceptionMessage('The required option "access_token" is missing');
+
+        $request = static::createRequest();
+        $request->resolve();
+    }
+
+    public function testAccessTokenInvalidException(): void
+    {
+        $this->expectException(InvalidOptionsException::class);
+        $this->expectExceptionMessage('The option "access_token" with value 123 is expected to be of type "string", but is of type "int"');
+
+        $request = static::createRequest();
+        $request->resolve(['access_token' => 123]);
+    }
+
+    public function testTypeInvalidException(): void
+    {
+        $this->expectException(InvalidOptionsException::class);
+        $this->expectExceptionMessage('The option "type" with value "bar" is invalid. Accepted values are: "jsapi", "wx_card"');
+
+        $request = static::createRequest();
+        $request->resolve(['access_token' => 'foo', 'type' => 'bar']);
+    }
+
+    public static function createRequest(): Ticket
+    {
+        $cachePool = new FilesystemAdapter();
+        $cachePool->clear();
+
+        return new Ticket($cachePool);
+    }
+}

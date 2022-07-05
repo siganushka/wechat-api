3db6be76c451bda58a47337e73fd368c7ff81760
siganushka <siganushka@gmail.com>
2022-07-05 15:56:51 +0800

    docs: update example

--- example/jsapi_config_utils.php
+++ example/jsapi_config_utils.php
@@ -3,11 +3,15 @@
 declare(strict_types=1);
 
 use Siganushka\ApiClient\Wechat\Jsapi\ConfigUtils;
+use Siganushka\ApiClient\Wechat\Ticket\Ticket;
 
 require __DIR__.'/_autoload.php';
+
+/** @var array{ ticket: string } */
+$parsedResponse = $client->send(Ticket::class);
 
 $apis = ['chooseImage'];
 
 $configUtils = new ConfigUtils($configuration);
-$config = $configUtils->generate('foo', $apis, true);
+$config = $configUtils->generate($parsedResponse['ticket'], $apis, true);
 dd($config);
